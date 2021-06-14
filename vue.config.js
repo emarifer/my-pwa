2283bed0b0b604eaec0573d127f769be0ff42136
emarinfer <enriquemarin_sierra@hotmail.com>
2021-06-14 07:52:45 +0200

    Change color button scroll to top

--- vue.config.js
+++ vue.config.js
@@ -66,6 +66,20 @@ module.exports = {
  * https://stackoverflow.com/questions/37896484/multiple-conditions-for-javascript-includes-method#43615512
  */
 
+/**
+ * MANEJO DE ACTUALIZACIONES DEL SERVICE WORKER EN UNA PWA CON VUE.JS. VER:
+ * https://dev.to/drbragg/handling-service-worker-updates-in-your-vue-pwa-1pip
+ * 
+ * UNA SOLUCION MENOS ELEGANTE (NO USA VUETIFY):
+ * https://levelup.gitconnected.com/vue-pwa-example-298a8ea953c9
+ * 
+ * CÓMO PROPORCIONAR SU PROPIA EXPERIENCIA DE INSTALACIÓN EN LA APLICACIÓN. VER:
+ * https://web.dev/customize-install/
+ * 
+ * COMANDO LINUX PARA ENTRAR DIRECTAMENTE EN LA CARPETA CREADA CON MKDIR:
+ * https://unix.stackexchange.com/questions/125385/combined-mkdir-and-cd#125459
+ */
+
  /**
   * 
   * ANTERIOR CONFIGURACION DE VUE.CONFIG.JS:
